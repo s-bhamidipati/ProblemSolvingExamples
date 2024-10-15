93dd7f8d7703cfd13a96bf5ac6ed7ca85a8802a6
sbhamidipati <sbhamidipati@SBHAMIDIPA-XEN>
2024-10-15 11:12:41 +0530

    added files and added a problem to find the possible combinations for given sum from a list of array

--- ReverseofString.cs
+++ ReverseofString.cs
@@ -24,6 +24,70 @@ namespace ProblemSolvingExamples
             }
 
             Console.WriteLine(reverse);
+
+            int[] arr = { 2, 3, 1, 4, 5, 6, 9, 10, 12, 23 };
+            List<int> A = new List<int>(arr);
+
+            int K = 18;
+
+            // Function call
+            Combination(A, K);
         }
+
+
+        static void unique_combination(int l, int sum, int K,
+                                   List<int> local,
+                                   List<int> A)
+        {
+            // If a unique combination is found
+            if (sum == K)
+            {
+                Console.Write("{");
+                for (int i = 0; i < local.Count; i++)
+                {
+                    if (i != 0)
+                        Console.Write(" ");
+                    Console.Write(local[i]);
+                    if (i != local.Count - 1)
+                        Console.Write(", ");
+                }
+                Console.WriteLine("}");
+                return;
+            }
+
+            // For all other combinations
+            for (int i = l; i < A.Count; i++)
+            {
+                // Check if the sum exceeds K
+                if (sum + A[i] > K)
+                    continue;
+
+                // Check if it is repeated or not
+                if (i > l && A[i] == A[i - 1])
+                    continue;
+
+                // Take the element into the combination
+                local.Add(A[i]);
+
+                // Recursive call
+                unique_combination(i + 1, sum + A[i], K, local,
+                                   A);
+
+                // Remove element from the combination
+                local.RemoveAt(local.Count - 1);
+            }
+        }
+
+        static void Combination(List<int> A, int K)
+        {
+            // Sort the given elements
+            A.Sort();
+
+            // To store combination
+            List<int> local = new List<int>();
+
+            unique_combination(0, 0, K, local, A);
+        }
+
     }
 }
---
+++ Sumofarrays.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingExamples
+{
+    class Sumofarrays
+    {
+        public static void Main()
+        {
+            //int[] arr = { 2, 3, 1, 4, 5, 6, 9, 10, 12, 23 };
+            //int givensum = 18;
+
+            //int[] newarr = { };
+
+            //for (int i = 0; i <= arr.Length-1; i++)
+            //{
+            //    newarr = newarr.Append(arr[i]).ToArray();
+            //    if (newarr.Sum() == givensum)
+            //    {
+
+            //        break;
+            //    }
+            //}
+
+            ////differentways to show array
+            //Array.ForEach(newarr, Console.WriteLine);
+            //Console.WriteLine("[{0}]", string.Join(",", newarr));
+            //newarr.ToList().ForEach(i=>Console.WriteLine(i.ToString()));
+
+            //findThePairs(new int[] { 2, 3, 1, 4, 5, 6, 9, 10, 12, 23 }, 18);
+
+            //findThePairs(new int[]{ 4, 6, 5, -10, 8, 5, 20 }, 10);
+
+            //findThePairs(new int[] { 4, -5, 9, 11, 25, 13, 12, 8 }, 20);
+
+            //findThePairs(new int[] { 12, 13, 40, 15, 8, 10, -15 }, 25);
+
+            //findThePairs(new int[] { 12, 23, 125, 41, -75, 38, 27, 11 }, 50);
+
+
+            List<int> lst = new List<int> { 2, 3, 1, 4, 5, 6, 9, 10, 12, 23 };
+            int i_sum = 18;
+            HashSet<string> match_sum = new HashSet<string>();
+            //int[] match_arr = { };
+
+            for (int i = 1; i < lst.Count; i++)
+            {
+                var combinations = GetCombinations(lst, i);
+                foreach (var matches in combinations)
+                {
+                    int tmp = matches.Sum();
+                    if (tmp == i_sum)
+                    {
+                        match_sum.Add(string.Join(", ", matches));
+                        //for(int j=0; j<matches)
+                      //  match_arr = match_arr.Append<int>(matches.FirstOrDefault()).ToArray();
+                    }
+                }
+            }
+
+            foreach (var match in match_sum.OrderBy(x => x))
+            {
+               // Console.WriteLine(match);
+                Console.WriteLine("[{0}]", string.Join(",", match));
+            }
+            //foreach (var match in match_arr.OrderBy(x => x))
+            //{
+            //    Console.WriteLine(match);
+            //}
+        }
+
+        static void findThePairs(int[] inputArray, int inputNumber)
+        {
+            //int givennumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pairs of elements whose sum is " + inputNumber + " are : ");
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] + inputArray[j] == inputNumber)
+                    {
+                        Console.WriteLine(inputArray[i] + " + " + inputArray[j] + " = " + inputNumber);
+                    }
+                }
+            }
+        }
+
+
+        static IEnumerable<IEnumerable<T>> GetCombinations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 0)
+                return new[] { new T[0] };
+
+            return list.SelectMany((e, i) =>
+                GetCombinations(list.Skip(i + 1), length - 1)
+                    .Select(c => new[] { e }.Concat(c)));
+        }
+
+
+    }
+}
